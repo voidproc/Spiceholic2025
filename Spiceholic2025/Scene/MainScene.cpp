1e3eac8c0e0625a6e8814fc99a178922d8401e4c
voidProc <voidproc@gmail.com>
2025-10-18 21:04:00 +0900

    ゲージ(WIP)

--- Spiceholic2025/Scene/MainScene.cpp
+++ Spiceholic2025/Scene/MainScene.cpp
@@ -22,9 +22,20 @@ namespace Spiceholic
 		// BG
 		SceneRect.draw(Palette::Darkolivegreen.lerp(Palette::Cyan, 0.5));//
 
-		// プレイヤー（暫定）
+		// プレイヤー
 		int charaAnimFrame = 4 + Periodic::Square0_1(0.9s);
 		TextureAsset(U"Chara")(charaAnimFrame * 48, 0, 48).drawAt(SceneCenter);
-	}
 
+		// HUD
+		{
+			// BG (0, 0), (0, 176)
+			Rect{ 0, 0, Size{ SceneSize.x, 16 } }.draw(ColorF{ 0, 0.5 });
+			Rect{ 0, 176, Size{ SceneSize.x, 16 } }.draw(ColorF{ 0, 0.5 });
+
+			// ゲージ枠、ゲージ
+			const Vec2 gaugePos{ 60, 174 };
+			TextureAsset(U"Gauge").draw(gaugePos + Vec2{ 21, 7 });
+			TextureAsset(U"GaugeFrame").draw(gaugePos);
+		}
+	}
 }
